5c6b3719580d69c627bf78e1bb74144e22471467
keroken <kenichi.shinagawa@gmail.com>
2024-10-06 23:29:07 +0900

    add routings for SignIn and Welcome

--- welcome/src/main.js
+++ welcome/src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Welcome from './pages/Welcome.vue';
+import SignIn from './pages/SignIn.vue';
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  // This is the part in common for all the routes
+  base: 'hello',
+  routes: [
+    { path: '/signin', component: SignIn },
+    { path: '/', component: Welcome },
+    { path: '*', redirect: '/' },
+  ],
+)};
 
 new Vue({
   render: h => h(App),
