5f115ba0782fedbfe8bade3018220b2a0cf16758
keroken <kenichi.shinagawa@gmail.com>
2024-10-06 23:39:51 +0900

    add NotFoundPage

---
+++ welcome/src/pages/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Not Found</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundPage',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h2 {
+  margin: 40px 0;
+}
+</style>
