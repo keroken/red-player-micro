f05f209c52e720dd6462922c47b73c584f372e59
keroken <kenichi.shinagawa@gmail.com>
2024-10-06 23:33:13 +0900

    update page names

--- welcome/src/pages/SignIn.vue
+++ welcome/src/pages/SignIn.vue
@@ -7,7 +7,7 @@
 
 <script>
 export default {
-  name: 'SignIn',
+  name: 'SignInPage',
 }
 </script>
 
--- welcome/src/pages/Welcome.vue
+++ welcome/src/pages/Welcome.vue
@@ -7,7 +7,7 @@
 
 <script>
 export default {
-  name: 'Welcome',
+  name: 'WelcomePage',
 }
 </script>
 
