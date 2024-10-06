091f666ff96a80f1e8d0e7a3169524261a769e63
keroken <kenichi.shinagawa@gmail.com>
2024-10-06 23:23:19 +0900

    add pages directory - SignIn & Welcome

---
+++ welcome/src/pages/SignIn.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Sign In</h1>
+    <h2>Please sign in</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignIn',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0;
+} 
+</style>
---
+++ welcome/src/pages/Welcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Welcome</h1>
+    <h2>Welcome to Red Player!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h2 {
+  margin: 40px 0;
+}
+</style>
