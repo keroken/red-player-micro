4ad2ea2c78ca348ce5fcaf3c6e8c85e89cd05a6a
keroken <kenichi.shinagawa@gmail.com>
2024-10-12 00:31:10 +0900

    update server.js - fix static url

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
-
-app.use('/mfe/welcome/', express.static('welcome/dist/'));
-
-app.use('/mfe/music/', express.static('music/dist/'));
+app.use('/', express.static('./public'));
+app.use('/mfe/welcome/', express.static('./welcome/dist/'));
+app.use('/mfe/music/', express.static('./music/dist/'));
 
 app.listen(port, () => {
   console.log(`Static server is running at http://localhost:${port}`);
